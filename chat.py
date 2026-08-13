513f2fb15301b7110c58a2d9faa80b785b1bfd53
Juho <juh.yla-outi@hotmail.fi>
2026-08-14 00:07:33 +0300

    Fixed chat.py device_map and added requirement package accelerate

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 import os
+import accelerate
 from getpass import getpass
 os.environ["HF_HUB_DISABLE_XET"] = "1" # Disable XET as it can slow down downloads.
 
@@ -26,7 +27,7 @@ f.close()
 
 # instantiate tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
 
 print("\nWelcome to Iarrki Chat! This AI tool is powered by Qwen3. Type 'T' to activate thinking mode.")
 thinking_mode = False
@@ -48,35 +49,13 @@ while True:
         messages = [
             {"role": "user", "content": prompt}
         ]
-        text = tokenizer.apply_chat_template(
+        inputs = tokenizer.apply_chat_template(
             messages,
-            tokenize=False,
             add_generation_prompt=True,
-            enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
-        )
-        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+            tokenize=True,
+            return_dict=True,
+            return_tensors="pt",
+        ).to(model.device)
 
-        # conduct text completion
-        generated_ids = model.generate(
-            **model_inputs,
-            max_new_tokens=32768
-        )
-        output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
-
-        # parsing thinking content
-        try:
-            # rindex finding 151668 (</think>)
-            index = len(output_ids) - output_ids[::-1].index(151668)
-        except ValueError:
-            index = 0
-
-        if thinking_mode:
-            thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
-            content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
-
-            print("\rThinking content:", thinking_content)
-            print("Content:", content)
-        else:
-            content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
-
-            print("\r", content)
+        outputs = model.generate(**inputs, max_new_tokens=40)
+        print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
